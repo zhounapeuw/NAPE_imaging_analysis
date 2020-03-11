92adb3662e8515e6b267bf52dac9f8123df883c6
zhounapeuw <56085812+zhounapeuw@users.noreply.github.com>
2020-03-11 10:34:06 -0700

    comments and added fill_gaps
    also added request for string input in main script
    
    added sima_motion_correction code

--- sima_motion_correct_batch.py
+++ sima_motion_correct_batch.py
@@ -4,61 +4,74 @@
 # In[ ]:
 
 
-
 """
+
 Finds any .tif, .tiff, .h5 files in the requested directory and performs SIMA-based motion correction and fft-based bidirection 
 offset correction. This code parallelizes the computation at the session level by passing the multiple file paths (if there are 
 more than one recordings) to the multiprocessing map function. The script sima_motion_correction contains the wrapping
 code for SIMA and the custom-created class for bidirection offset correction.
 
-See these documentations for details:
+Two simple ways to execute this in command line:  
+A) sima_motion_correct_batch; then input the path_to_directory
+
+B) sima_motion_correct_batch.batch_process(path_to_directory)
+
+See these documentations for details
+------------------------------------
 
 https://github.com/losonczylab/sima
 http://www.losonczylab.org/sima/1.3.2/
 https://www.frontiersin.org/articles/10.3389/fninf.2014.00080/full
 
-required packages: 
+Required Packages
+-----------------
 sima, glob, multiprocessing, numpy, h5py, pickle (optional if want to save displacement file) 
+
+Custom code requirements: sima_motion_correction, bidi_offset_correction
 
 Parameters
 ----------
 
 fdir : string
-    root file directory. 
+    root file directory containing the raw tif, tiff, h5 files 
+
+Optional Parameters
+-------------------
 
 max_disp : list of two entries
     Each entry is an int. First entry is the y-axis maximum allowed displacement, second is the x-axis max allowed displacement.
-    The shift for each line cannot go above these values.
-
+    The number of pixel shift for each line cannot go above these values.
+    Note: 50 pixels is approximately 10% of the FOV (512x512 pixels)
+    
+    Defaults to [30, 50]
+    
 save_displacement : bool 
     Whether or not to have SIMA save the calculated displacements over time. def: False; NOTE: if this is switched to True,
     it can double the time to perform motion correction.
-
+    
+    Defaults to False
+    
 Output
 -------
 motion corrected file (in the format of h5) with "_sima_mc" appended to the end of the file name
     
 
-"""
+""";
 
 
 # In[ ]:
 
 
-import sima_motion_correction
-reload(sima_motion_correction)
+import sima_motion_correction # reload(sima_motion_correction)
 import glob
 import multiprocessing as mp
+import os
 
 
 # In[ ]:
 
 
 def batch_process(fdir, max_disp = [30, 50], save_displacement = False):
-
-    fdir = 'C:\\2pData\\Sean\\'
-    max_disp = [30, 50] 
-    save_displacement = False
 
     # gather all tif or h5 files in root directory
     fpaths = []
@@ -67,11 +80,15 @@ def batch_process(fdir, max_disp = [30, 50], save_displacement = False):
         fpaths.extend(glob.glob(fdir + type))
     print(str(len(fpaths)) + ' files to analyze')
 
-    # perform parallel processing
+    if len(fpaths) == 0:
+        print("No files to analyze!")
+    
+    # determine number of cores to use and initialize parallel pool
     num_processes = min(mp.cpu_count(), len(fpaths))
     print( 'Total CPU cores for parallel processing: ' + str(num_processes) )
-
     pool = mp.Pool(processes = num_processes)
+    
+    # perform parallel processing; pass iterable list of file paths to the motion correction script
     pool.map(sima_motion_correction.unpack, [(file, max_disp) for file in fpaths])
     pool.close()
     pool.join()
@@ -80,13 +97,5 @@ def batch_process(fdir, max_disp = [30, 50], save_displacement = False):
 # In[ ]:
 
 
-if __name__ == "__main__":
-    fdir = raw_input()
-    batch_process(fdir)
-
-
-# In[ ]:
-
-
-
+get_ipython().run_cell_magic(u'time', u'', u'if __name__ == "__main__":\n    fdir = raw_input("Input root directory of tif, tiff, h5 files to analyze; note: leave the last backlash off")\n    batch_process(fdir + \'\\\\\')')
 
---
+++ sima_motion_correction.py
@@ -0,0 +1,80 @@
+import sima
+import sima.motion
+import numpy as np
+import os
+import pickle
+import h5py
+import sys
+from sima import sequence
+sys.path.insert(1, 'C:\\Users\\stuberadmin\\Dropbox (Stuber Lab)\\Python\\Charles\\')
+import bidi_offset_correction
+from contextlib import contextmanager
+
+def unpack(args):
+    print(args)
+    return sima_motion_correction(*args)
+
+def sima_motion_correction(fpath, max_disp, save_displacement=False):
+    print('sima_motion_correction')
+    fdir  = os.path.split(fpath)[0]
+    fname = os.path.splitext(os.path.split(fpath)[1])[0]
+    fext  = os.path.splitext(os.path.split(fpath)[1])[1]
+
+    if fext == '.tif' or fext == '.tiff':
+        # sequence: object that contains record of whole dataset; data not stored into memory all at once
+        sequences = [sima.Sequence.create('TIFF', fpath)]
+
+    elif fext == '.h5':
+        sequences = [sima.Sequence.create('HDF5', fpath, 'tyx')]
+
+    # define motion correction method
+    # n_processes can only handle =1! Bug in their code where >1 runs into an error
+    # max_displacement: The maximum allowed displacement magnitudes in pixels in [y,x]
+    mc_approach = sima.motion.HiddenMarkov2D(granularity='row', max_displacement=max_disp, n_processes=1, verbose=True)
+
+    # apply motion correction to data
+    dataset, __, _ = mc_approach.correct(sequences, os.path.join(fdir, fname + '_mc.sima'), channel_names=['GCaMP'],
+                                         trim_criterion=0.1)
+
+    # use sima's fill_gaps function to interpolate missing data from motion correction
+    # dtype can be changed to int16 since none of values are floats
+    data_mc = np.empty(dataset[0]._sequences[0].shape, dtype='int16')
+    filled_data = sequence._fill_gaps(iter(dataset[0]._sequences[0]), iter(dataset[0]._sequences[0]))
+    for f_idx, frame in enumerate(filled_data):
+        data_mc[f_idx, ...] = frame
+    data_mc = np.squeeze(data_mc)
+
+    # dataset dimensions are frame, plane, row(y), column (x), channel
+
+    if save_displacement is True:
+        # show motion displacements after motion correction
+        mcDisp_approach = sima.motion.HiddenMarkov2D(granularity='row', max_displacement=max_disp, n_processes=1,
+                                                     verbose=True)
+        displacements = mcDisp_approach.estimate(dataset)
+
+        # save the resulting displacement file
+        # only useful if you want to see the values of displacement calculated by SIMA to perform the motion correction
+        displacement_file = open(os.path.join(fdir, fname + '_mc.sima/displacement.pkl'), "wb")
+        pickle.dump(displacements, displacement_file)
+        displacement_file.close()
+
+        # process and save np array of composite displacement
+        data_dims = displacements[0].shape
+        disp_np = np.squeeze(np.array(displacements[0]))
+        disp_meanpix = np.mean(disp_np, axis=1)  # avg across lines (y axis)
+
+        sima_disp = np.sqrt(
+            np.square(disp_meanpix[:, 0]) + np.square(disp_meanpix[:, 1]))  # calculate composite x + y offsets
+        np.save(os.path.join(fdir, 'displacements\\displacements_sima.npy'), sima_disp)
+
+    # perform bidirection offset correction
+    my_bidi_corr_obj = bidi_offset_correction.bidi_offset_correction(data_mc)  # initialize data to object
+    my_bidi_corr_obj.compute_mean_image()  # compute mean image across time
+    my_bidi_corr_obj.determine_bidi_offset()  # calculated bidirectional offset via fft cross-correlation
+    data_corrected = my_bidi_corr_obj.correct_bidi_frames()  # apply bidi offset to data
+
+    # save motion-corrected, bidi offset corrected dataset
+    sima_mc_bidi_outpath = os.path.join(fdir, fname + '_sima_mc.h5')
+    h5_write_bidi_corr = h5py.File(sima_mc_bidi_outpath, 'w')
+    h5_write_bidi_corr.create_dataset('imaging', data=data_corrected)
+    h5_write_bidi_corr.close()
